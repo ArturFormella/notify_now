a0c3689e5e6eecc39dd35c3e6597d8cf536f957e
Artur Formella <biuro@arczi.info>
2022-12-23 19:18:16 +0100

    Repaired white characters

--- notify_now.c
+++ notify_now.c
@@ -4,7 +4,7 @@
  *     extenstion for PostgreSQL
  *
  * IDENTIFICATION
- *		notify_now/notify_now.c
+ *  notify_now/notify_now.c
  *
  *-------------------------------------------------------------------------
  */
@@ -28,42 +28,43 @@ PG_FUNCTION_INFO_V1(notify_now);
 Datum
 notify_now(PG_FUNCTION_ARGS)
 {
-	size_t		channel_len;
-	size_t		payload_len;
-	const char *channel;
-	const char *payload;
+  size_t  channel_len;
+  size_t  payload_len;
+  const char *channel;
+  const char *payload;
   StringInfoData buf;
 
-	if (PG_ARGISNULL(0))
-		channel = "";
-	else
-		channel = text_to_cstring(PG_GETARG_TEXT_P(0));
+  if (IsParallelWorker())
+    elog(ERROR, "cannot send notifications from a parallel worker");
 
-	if (PG_ARGISNULL(1))
-		payload = "";
-	else
-		payload = text_to_cstring(PG_GETARG_TEXT_P(1));
+  if (PG_ARGISNULL(0))
+    channel = "";
+  else
+    channel = text_to_cstring(PG_GETARG_TEXT_P(0));
 
-	channel_len = strlen(channel);
-	payload_len = strlen(payload);
+  if (PG_ARGISNULL(1))
+    payload = "";
+  else
+    payload = text_to_cstring(PG_GETARG_TEXT_P(1));
+
+  channel_len = strlen(channel);
+  payload_len = strlen(payload);
  
-	if (channel_len == 0)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("channel name cannot be empty")));
+  if (channel_len == 0)
+    ereport(ERROR,
+        (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+         errmsg("channel name cannot be empty")));
 
-	if (channel_len >= NAMEDATALEN)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("channel name too long")));
+  if (channel_len >= NAMEDATALEN)
+    ereport(ERROR,
+        (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+         errmsg("channel name too long")));
 
-	if (IsParallelWorker())
-		elog(ERROR, "cannot send notifications from a parallel worker");
-  
-	if (payload_len >= NOTIFY_NOW_PAYLOAD_MAX_LENGTH)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("payload string too long")));
+
+  if (payload_len >= NOTIFY_NOW_PAYLOAD_MAX_LENGTH)
+    ereport(ERROR,
+        (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+         errmsg("payload string too long")));
 
   pq_beginmessage(&buf, 'A'); // NOTIFY
   pq_sendint32(&buf, MyProcPid);
@@ -71,5 +72,5 @@ notify_now(PG_FUNCTION_ARGS)
   pq_sendstring(&buf, payload);
   pq_endmessage(&buf);
   pq_flush(); // send as soon as possible
-	PG_RETURN_VOID();
+  PG_RETURN_VOID();
 }
