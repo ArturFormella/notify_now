bdd38478de89d6e962f7b5bf18bd1b58f0cf4243
Artur Formella <biuro@arczi.info>
2022-12-24 00:25:07 +0100

    Fix line endings. Added more comments

--- example_node_app/index.js
+++ example_node_app/index.js
@@ -9,20 +9,26 @@ var client = new pg.Client({
   });
 client.connect();
 
-
+// Abstraction layer
 const queryWithNotify = async (client, onMessage, query, args = null) => {
   try {
+    // LISTEN for messages using libpq API but don't have to run LISTEN to inform Postgres.
     client.on('notification', onMessage);
     return await client.query(query, args);
   } finally {
+    // remove listener from libpq. Don't havee to run UNLISTEN.
     client.removeListener("notification", onMessage);
   }
 };
+
 const prepare = async () => {
-  await client.query(`DROP EXTENSION IF EXISTS notify_now`);
   await client.query(`CREATE EXTENSION IF NOT EXISTS notify_now`);
 };
 
+
+/*
+  Baic example - return any part of CTE. Not only the main result.
+*/
 const example1 = async () => {
   console.log('-------------------------------EXAMPLE 1----------------------------------------');
   const start = Date.now();
@@ -42,10 +48,17 @@ const example1 = async () => {
   ), y as (
     SELECT * FROM notify_now('counter2', ((SELECT json_agg(row_to_json(x)) FROM x))::text)
   )
-  select * from z, y
+  SELECT * FROM z, y
   `);
+  
   console.log('main result:', res.rows);
 };
+
+
+/*
+  Returns main result, query plan and additional messages.
+  pg_sleep simulate heavy computations.
+*/
 
 const example2 = async () => {
   console.log('-------------------------------EXAMPLE 2----------------------------------------');
@@ -53,32 +66,37 @@ const example2 = async () => {
 
   const eventReceiver = (data) => {
     const stop = Date.now();
-    console.log(`example2 - channel: ${data.channel},\tlength: ${data.payload.length}\ttime: ${(stop - start)}`);
+    console.log(`example2 - channel: ${data.channel},\tlength: ${data.payload.length} Bytes\ttime: ${(stop - start)}`);
   };
 
   const res = await queryWithNotify(client, eventReceiver, `
   EXPLAIN ANALYZE
   SELECT * FROM
-      generate_series(80,85) as s(num), 
-      notify_now('channel' || num, repeat('#', num*num*num) ), 
-      pg_sleep(num/num);`);
+    generate_series(80,85) as s(num), 
+    notify_now('channel' || num, repeat('#', num*num*num) ), 
+    pg_sleep(num/num);`);
 
-  console.log('main result:', res.rows);
+  console.log('main result:', res.rows.map( r => r['QUERY PLAN']).join('\n'));
 };
+
+/*
+  Return multiple datasets from PROCEDURE.
+  pg_sleep simulate heavy computations.
+*/
 
 const example3 = async () => {
   console.log('-------------------------------EXAMPLE 3----------------------------------------');
 
-  await client.query(`create or replace procedure public.example()
-    language plpgsql
-    as $$
+  await client.query(`CREATE OR REPLACE PROCEDURE public.example()
+    LANGUAGE plpgsql
+    AS $$
     begin
-        Perform 1 FROM notify_now('counter1', ((SELECT json_agg(x) FROM generate_series(1,5) as x(pos)))::text);
-        Perform pg_sleep(1);
-        Perform 1 FROM notify_now('counter2', ((SELECT json_agg(x) FROM generate_series(6,10) as x(pos)))::text);
-        Perform pg_sleep(1);
-        Perform 1 FROM notify_now('counter3', ((SELECT json_agg(x) FROM generate_series(11,15) as x(pos)))::text);
-        Perform pg_sleep(1);
+        PERFORM 1 FROM notify_now('counter1', ((SELECT json_agg(x) FROM generate_series(1,5) as x(pos)))::text);
+        PERFORM pg_sleep(1);
+        PERFORM 1 FROM notify_now('counter2', ((SELECT json_agg(x) FROM generate_series(6,10) as x(pos)))::text);
+        PERFORM pg_sleep(1);
+        PERFORM 1 FROM notify_now('counter3', ((SELECT json_agg(x) FROM generate_series(11,15) as x(pos)))::text);
+        PERFORM pg_sleep(1);
     end;$$`);
 
   const eventReceiver = (data) => {
@@ -93,21 +111,12 @@ const example3 = async () => {
 
 
 (async function run() {
- 
+
   await prepare();
+
   await example1();
   await example2();
   await example3();
 
   client.end();
 })();
-
-
-/*
-jest payload: 811,      533411731       time: 2154152 time: 10286
-jest payload: 812,      535387328       time: 2164482 time: 10330
-node:buffer:594
-    slice: (buf, start, end) => buf.utf8Slice(start, end),
-
-  SELECT notify_now('counter', x) FROM x
-*/
